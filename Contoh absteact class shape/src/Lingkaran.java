385d8543f1089d003d7e388f3a9e7811dbe93926
NOpaa <136408010+nopp12@users.noreply.github.com>
2023-11-25 01:27:18 +0700

    Delete Contoh absteact class shape directory

--- Contoh absteact class shape/src/Lingkaran.java
+++
@@ -1,9 +0,0 @@
-class Lingkaran extends Shape {
-    public Lingkaran(String nama, String warna) {
-        super(nama, warna);
-    }
-
-    public void draw() {
-        System.out.println("Menggambar " + getNama() + " berwarna " + getWarna());
-    }
-}
--- Contoh absteact class shape/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Lingkaran lingkaran = new Lingkaran("Lingkaran", "Cyan");
-        lingkaran.draw();
-    }
-}
