25cfe8d78fba459d4ba8c457cd571437d2aa387d
NOpaa <136408010+nopp12@users.noreply.github.com>
2023-11-24 23:08:48 +0700

    Add files via upload

---
+++ contoh abstract class elektronik/src/ElectronicDevice.java
@@ -0,0 +1,5 @@
+abstract class ElectronicDevice {
+    public abstract void turn_on();
+    public abstract void turn_off();
+    public abstract void set_timer(int menit);
+}
---
+++ contoh abstract class game/src/GameCatur.java
@@ -0,0 +1,13 @@
+class GameCatur extends Game {
+    public void start() {
+        System.out.println("Permainan catur dimulai.");
+    }
+
+    public void pause() {
+        System.out.println("Permainan catur dijeda.");
+    }
+
+    public void game_over() {
+        System.out.println("Permainan catur selesai.");
+    }
+}
