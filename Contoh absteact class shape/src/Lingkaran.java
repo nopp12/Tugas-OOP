b179f6af766e5b29f35fb983fb1131797b94470b
NOpaa <136408010+nopp12@users.noreply.github.com>
2023-11-25 01:18:55 +0700

    Add files via upload

--- Contoh absteact class shape/src/Lingkaran.java
+++ Contoh absteact class shape/src/Lingkaran.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Lingkaran extends Shape {
     public Lingkaran(String nama, String warna) {
         super(nama, warna);
@@ -7,14 +6,4 @@ class Lingkaran extends Shape {
     public void draw() {
         System.out.println("Menggambar " + getNama() + " berwarna " + getWarna());
     }
-=======
-class Lingkaran extends Shape {
-    public Lingkaran(String nama, String warna) {
-        super(nama, warna);
-    }
-
-    public void draw() {
-        System.out.println("Menggambar " + getNama() + " berwarna " + getWarna());
-    }
->>>>>>> 0de9f9a02b7c32841af295b60b5ed667dffff3a1
 }
